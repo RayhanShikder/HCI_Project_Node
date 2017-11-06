649e22195420186d7679dc33bca7e4d1ef4bd0b5
Rayhan Shikder <rayhanshikder@Rayhans-MBP-2.hitronhub.home>
2017-11-05 19:51:50 -0600

    basic skeleton developed

---
+++ api/routes/videoListRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var videoList = require('../controllers/videoListController');
+
+  // todoList Routes
+  app.route('/videos')
+    .get(videoList.list_all_videos)
+    .post(videoList.create_a_video); //do later
+
+
+  app.route('/videos/:videoId')
+    .get(videoList.read_a_video)
+   // .put(videoList.update_a_video) //do later
+    //.delete(videoList.delete_a_video); //do later
+};
