fb99afee517e279a9043796cad19ebc419ce95f4
Rayhan Shikder <rayhanshikder@Rayhans-MBP-2.hitronhub.home>
2017-11-06 00:03:32 -0600

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+var cors = require('cors');
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 4000,
   mongoose = require('mongoose'),
   Task = require('./api/models/videoListModel'), //created model loading here
   bodyParser = require('body-parser');
@@ -12,17 +13,19 @@ mongoose.connect('mongodb://localhost/VideoDb');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors()); 
 
-app.use(function(req, res) { //for undefined urls
-  res.status(404).send({url: req.originalUrl + ' not found'})
-});
 
 
 var routes = require('./api/routes/videoListRoutes'); //importing route
 routes(app); //register the route
 
+app.use(function(req, res) { //for undefined urls
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 app.listen(port);
 
 
 console.log('video list RESTful API server started on: ' + port);
+
